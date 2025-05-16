acce8029aa762438ddd0c97aad719d3225f7dbfb
Joseph Ales <162182688+josephiii@users.noreply.github.com>
2025-05-16 16:39:45 -0400

    register.js input validation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # COP3441-ContactManager
+
+Small Project: Contact Manager for UCF COP4331 - Group 2 - Summer 2025
---
+++ js/register.js
@@ -0,0 +1,80 @@
+
+const urlBase = 'http://orbitcontacts.xyz';
+const extension = 'php';
+
+
+function doRegister(){
+
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+
+    const registerError = validateRegister(firstName, lastName, username, password, confirmPassword);
+
+    if(registerError){
+        document.getElementById('registerError').innerHTML = registerError;
+        return;
+    }
+    
+    //var passwordHash = md5(password);         this is optional
+    
+    const payload = {
+        'firstName': firstName,
+        'lastName': lastName,
+        'username': username,
+        'password': password
+    }
+
+    //implement the fetch or XMLHttpRequest
+
+}
+
+
+function validateRegister(firstName, lastName, username, password, confirmPassword){
+
+    if(!firstName || !lastName || !username || !password || !confirmPassword){
+        return 'All fields required';
+    }
+
+    if(!pwdStrengthCheck(password)){
+        return 'Passwords must have at least 8 characters, an uppercase letter, a lowercase letter, and a number';
+    }
+
+    if(password !== confirmPassword){
+        return 'Passwords must match';
+    }
+
+    return null;
+}
+
+
+function pwdStrengthCheck(password){
+
+    if(password.length < 8){
+        return false;
+    }
+
+    var uppercase = false;
+    var lowercase = false;
+    var number = false;
+
+    for(let i=0; i<password.length; i++){
+        let char = password[i];
+
+        if(/[A-Z]/.test(char)){
+            uppercase = true;
+        } else if (/[a-z]/.test(char)){
+            lowercase = true;
+        } else if (/[0-9]/.test(char)){
+            number = true;
+        } 
+    }
+
+    if(uppercase && lowercase && number){
+        return true;
+    }
+
+    return false;
+}
