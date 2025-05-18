6bbc9236580acf3bfc3066d0fd8ee5ff00c164f9
Duro233 <146140907+Duro233@users.noreply.github.com>
2025-05-18 11:26:18 -0400

    Update searchContact.php

--- LAMPAPI/searchContact.php
+++ LAMPAPI/searchContact.php
@@ -1,68 +1,62 @@
 <?php
 
-	$inData = getRequestInfo();
-	
-	$searchResults = "";
-	$searchCount = 0;
+$inData = getRequestInfo();
 
-	$conn = new mysqli('localhost', 'TheBeast', 'COP4331root', 'COP4331');
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("select Name from contact where Name like ? and UserID=?");
-		$contactName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $contactName, $inData["userId"]);
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
-		
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '"' . $row["Name"] . '"';
-		}
-		
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
-		
-		$stmt->close();
-		$conn->close();
-	}
+$searchResults = [];
+$conn = new mysqli('localhost', 'TheBeast', 'COP4331root', 'COP4331');
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $stmt = $conn->prepare("SELECT ID, firstName, lastName FROM contacts WHERE (firstName LIKE ? OR lastName LIKE ?) AND userId=?");
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    $searchTerm = "%" . $inData["search"] . "%";
+    $userId = $inData["userId"];
+    $stmt->bind_param("sss", $searchTerm, $searchTerm, $userId);
+    $stmt->execute();
+    
+    $result = $stmt->get_result();
+
+    while ($row = $result->fetch_assoc()) {
+        $searchResults[] = $row;
+    }
+
+    if (count($searchResults) === 0) {
+        returnWithError("No Records Found");
+    } else {
+        returnWithInfo($searchResults);
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo json_encode($obj);
+}
+
+function returnWithError($err)
+{
+    $retValue = [
+        "results" => [],
+        "error" => $err
+    ];
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($results)
+{
+    $retValue = [
+        "results" => $results,
+        "error" => ""
+    ];
+    sendResultInfoAsJson($retValue);
+}
 ?>
